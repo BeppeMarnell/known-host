ca65e6cfb6ace2d759d598052ace79e6502b2d60
beppemarinelli <marinellibeppe@gmail.com>
2024-09-17 10:34:06 +0200

    Improved script by deleting all lines

--- known-host.plugin.zsh
+++ known-host.plugin.zsh
@@ -13,6 +13,20 @@ known-host() {
 
     line_number="$2"
     tmp_file=$(mktemp)
-    sed "${line_number}d" ~/.ssh/known_hosts > "$tmp_file"
+
+    # Extract the IP address from the specified line
+    ip_address=$(sed "${line_number}q;d" ~/.ssh/known_hosts | awk '{print $1}')
+
+    if [ -z "$ip_address" ]; then
+        echo "No IP address found at line $line_number"
+        return 1
+    fi
+
+    echo "Removing all lines with IP address: $ip_address"
+
+    # Remove all lines containing the IP address
+    sed "/$ip_address/d" ~/.ssh/known_hosts > "$tmp_file"
+    
+    # Overwrite the known_hosts file with the updated content
     mv "$tmp_file" ~/.ssh/known_hosts
 }
